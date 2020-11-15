920ee981ae5653ef5deeeb3111adb81cb57554a4
Ihor Kharchenko <ihor.stavr@gmail.com>
2020-11-15 19:42:54 +0200

    task-2

--- js/task-2.js
+++ js/task-2.js
@@ -1,24 +1,20 @@
- // Write code under this line
-class User {
-  constructor(name, age, followers) {
-    this.name = name;
-  this.age = age;
-  this.followers = followers;
-}
-  
-getInfo (){
-return `User ${this.name} is ${this.age} years old and has ${this.followers} followers`}
-}
-console.log(typeof User);
-// 'function'
+const ingredientsList = document.getElementById('ingredients')
+// console.log(ingredientsList)
+const ingredients = [
+  'Картошка',
+  'Грибы',
+  'Чеснок',
+  'Помидоры',
+  'Зелень',
+  'Приправы',
+];
 
-const mango = new User('Mango', 2, 20);
- console.log(mango.getInfo()); 
-// 'User Mango is 2 years old and has 20 followers'
-
-console.log(typeof mango.getInfo); 
-// 'function'  
-
-const poly = new User( 'Poly', 3, 17);
-console.log(poly.getInfo());
-// 'User Poly is 3 years old and has 17 followers'
+function createListElement(array) { 
+  return array.map(item => {
+    const element = document.createElement('li');
+    element.textContent = item;
+    ingredientsList.append(element);
+    return ingredientsList;
+  });
+};
+const products = createListElement(ingredients);
